8f5c7ee665f74c7c73189f4180dc0fa785f1f2ec
Tahmina Ringer <tahmina.ringer@gmail.com>
2021-09-29 18:08:00 -0700

    added footer with link to github

--- src/index.js
+++ src/index.js
@@ -4,17 +4,29 @@ import axios from 'axios';
 
 const WeatherApp = () => {
   const [temp, setTemp] = useState('');
+  const [description, setDescription] = useState('');
+  const [icon, setIcon] = useState('');
   const [city, setCity] = useState('');
   const [country, setCountry] = useState('');
-  // const [country, setCountry] = useState('')
+ 
 
   const getWeatherData = (city) => {
     axios({
       method: 'GET',
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=85fc1bf2a00f88d138b289ff7009cd44`
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}/img/w/${icon}.png&APPID=85fc1bf2a00f88d138b289ff7009cd44`
     })
       .then((response) => {
-        console.log(response.data);
+        console.log(response.data.main.temp);
+        //retrieving info from API setting to state and 
+        //transfering Kelvin to Fahrenheit
+        setTemp((((response.data.main.temp) - 273.15)*1.8)+32)
+        //If wanted to convert Kelvin to celcius
+        //setTemp((response.data.main.temp) - 273.15)
+
+        //displays weather conditions ex: cloudy rain sunny etc.
+        setDescription(response.data.weather[0].main)
+        //set
+        setIcon(response.data.weather[0].icon)
       })
       .catch((error) => {
         console.log(error);
@@ -23,22 +35,51 @@ const WeatherApp = () => {
 
   return (
     <>
-      <div style={{ marginLeft: '33%' }}>
         <div
           style={{
-            height: '150px',
-            width: '450px',
-            backgroundColor: '#94e5ff',
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
-            fontSize: '25px',
+            height: '70px',
+            width: '100%',
+            backgroundColor: '#226ba3',
+            fontSize: '30px',
+            color: '#fff',
           }}
         >
+          Weather APP
+        </div>
+        <div
+          style={{
+            height: '5px',
+            width: '100%',
+            backgroundColor: 'black',
+          }}
+        ></div>
           <br />
+          <div style={{ marginLeft: '33%'}}>
+          <div
+            style={{
+              height: '150px',
+              width: '450px',
+              backgroundColor:'94e5ff',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontSize: '25px',
+            }}
+          >
+            {new Date().toLocaleString()}
+            <br />
           {city} Weather
           <br />
-          {/* {temperature} */}
+          {/* if converting to farhenheit */}
+          {Math.round(temp)} °F - {description}
+          {/* if converting to celcius */}
+          {/* {Math.round(temp)} °C */}
+          <br />
+          
+          {icon}
         </div>
         <br />
         <input
@@ -59,6 +100,34 @@ const WeatherApp = () => {
           GET
         </button>
       </div>
+      <div
+          style={{
+
+            height: '5px',
+            width: '100%',
+            backgroundColor: 'black',
+            marginTop: '175px',
+          }}
+        ></div>
+      <footer className='footer' 
+      style={{ 
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '125px',
+        width: '100%',
+        backgroundColor: 'grey',
+        fontSize: '15px',
+        color: '#fff',
+        }}>
+        <code>
+          Created by {' '}
+          <a href="https://github.com/tahminaringer" target='none'>
+            Tahmina Ringer
+          </a>{' '}
+          using React
+        </code>
+      </footer>
     </>
   );
 };
